a842a76cc386a6fd6e08f74983216e4359661278
YigitaliyevIslomjon <77922591+YigitaliyevIslomjon@users.noreply.github.com>
2023-02-19 01:50:51 +0500

    graphql congif is ready to use

---
+++ src/Services/apollo.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { API_URL } from "Utils/config";
+
+export const client = new ApolloClient({
+  uri: API_URL,
+  cache: new InMemoryCache(),
+});
---
+++ src/Utils/config.ts
@@ -0,0 +1,3 @@
+const production = true;
+
+export const API_URL = production ? process.env.REACT_APP_API_URL : "";
